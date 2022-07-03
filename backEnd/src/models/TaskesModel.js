667222624fff76d8458824b4fc6945e126fabd15
Rafael Amaral <amaral.avelar.filo@gmail.com>
2022-07-03 20:52:47 -0300

    feat(backend): backEnd functions done
    BREAKING CHANGE: starting frontEnd

--- backEnd/src/models/TaskesModel.js
+++ backEnd/src/models/TaskesModel.js
@@ -8,8 +8,8 @@ const getAll = async (userId) => {
 
 const create = async (task, status, actualDate, userId) => {
   const query = `INSERT INTO task_list_${userId}(task, status, date) VALUES (?, ?, ?)`;
-  const [taskList] = await connection.execute(query, [task, status, actualDate]);
-  return taskList;
+  const [resultSetHeader] = await connection.execute(query, [task, status, actualDate]);
+  return resultSetHeader;
 };
 
 const update = async (id, task, status, actualDate, userId) => {
@@ -20,14 +20,14 @@ const update = async (id, task, status, actualDate, userId) => {
     date=?
     WHERE id=?
   `;
-  const [taskList] = await connection.execute(query, [task, status, actualDate, id]);
-  return taskList;
+  const [resultSetHeader] = await connection.execute(query, [task, status, actualDate, id]);
+  return resultSetHeader;
 };
 
 const destroy = async (userId, id) => {
   const query = `DELETE FROM task_list_${userId} WHERE id=?`;
-  const [task] = await connection.execute(query, [id]);
-  return task;
+  const [resultSetHeader] = await connection.execute(query, [id]);
+  return resultSetHeader;
 };
 
 module.exports = {
