e3655ffdd5f1a83ba096eb52e1d9bfd8f6701ed8
Rafael Amaral <amaral.avelar.filo@gmail.com>
2022-07-02 22:16:06 -0300

    feat(lint): installing and configuring lint es

---
+++ backEnd/server.js
@@ -0,0 +1,2 @@
+const juntar = "asdasdasdsad";
+
--- backEnd/testing-database.sql
+++ backEnd/testing-database.sql
@@ -1,44 +1,55 @@
-CREATE DATABASE task_list_ebytr;
 
-USE task_list_ebytr ;
+CREATE database task_list_ebytr;
 
-CREATE TABLE IF NOT EXISTS users (
+CREATE TABLE IF NOT EXISTS `task_list_ebytr`.users (
 	id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
 	name VARCHAR(100) NOT NULL,
 	email VARCHAR(100),
-	password VARCHAR(100),
+	password VARCHAR(100)
 );
 
-CREATE TABLE IF NOT EXISTS task_list_1 (
+CREATE TABLE IF NOT EXISTS `task_list_ebytr`.task_list_1 (
 	id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
 	task VARCHAR(255) NOT NULL,
-  status VARCHAR(20) NOT NULL,
-	data DATETIME,
+    status VARCHAR(20) NOT NULL,
+	date DATETIME DEFAULT CURRENT_TIMESTAMP
 );
 
-CREATE TABLE IF NOT EXISTS task_list_2 (
+CREATE TABLE IF NOT EXISTS `task_list_ebytr`.task_list_2 (
 	id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
 	task VARCHAR(255) NOT NULL,
-  status VARCHAR(20) NOT NULL,
-	data DATETIME,
+    status VARCHAR(20) NOT NULL,
+	date DATETIME DEFAULT CURRENT_TIMESTAMP
 );
 
-CREATE TABLE IF NOT EXISTS task_list_3 (
+CREATE TABLE IF NOT EXISTS `task_list_ebytr`.task_list_3 (
 	id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
 	task VARCHAR(255) NOT NULL,
-  status VARCHAR(20) NOT NULL,
-	date DATETIME,
+    status VARCHAR(20) NOT NULL,
+	date DATETIME DEFAULT CURRENT_TIMESTAMP
 );
 
 INSERT INTO
-	users (name, email, password)
+	`task_list_ebytr`.users (name, email, password)
 VALUES
 	('Joao Magno', 'magno@email.com', 'magnotasks10'),
 	('Joao Vitor', 'vitor@email.com', 'vitortasks11'),
-	('Joao Carini', 'carini@email.com', 'carinitasks12'),
+	('Joao Carini', 'carini@email.com', 'carinitasks12');
 
 INSERT INTO
-	task_list_1 (task, status, date)
+	`task_list_ebytr`.task_list_1 (task, status, date)
 VALUES
-	('organizar o banco de dados', 'pendente', GETDATE()),
-	('configurar servidor', 'em andamento', GETDATE()),
+	('organizar o banco de dados', 'pendente', NOW()),
+	('configurar servidor', 'em andamento', NOW());
+    
+INSERT INTO
+	`task_list_ebytr`.task_list_2 (task, status, date)
+VALUES
+	('organizar o banco de dados', 'pendente', NOW()),
+	('configurar servidor', 'em andamento', NOW());
+    
+INSERT INTO
+	`task_list_ebytr`.task_list_3 (task, status, date)
+VALUES
+	('organizar o banco de dados', 'pendente', NOW()),
+	('configurar servidor', 'em andamento', NOW());
